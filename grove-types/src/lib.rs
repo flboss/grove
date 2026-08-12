2e9120c1bc7feb68c6b83fc4fd435d3a26d1b640
Florian Boss <f@bossf.dev>
2026-08-13 00:12:20 +0200

    feat(grove-types): implement PartialOrd, Ord on Span

--- grove-types/src/lib.rs
+++ grove-types/src/lib.rs
@@ -1,6 +1,6 @@
 use std::{borrow::Cow, ops::Range};
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
 pub struct Span {
     pub start: usize,
     pub end: usize,
